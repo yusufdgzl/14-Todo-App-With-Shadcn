8be3b21eae98dc4d044fa8d12fbd9d86f961720f
erdem <berdemozdemir@gmail.com>
2024-02-08 19:54:17 +0300

    refactor whole project

--- components/todo/todo-card.jsx
+++ components/todo/todo-card.jsx
@@ -1,7 +1,4 @@
-import { EditDialog } from "../alert/EditDialog";
-import { AlertDialogItem } from "../alert/alert-dialog";
 import { Separator } from "../ui/separator";
-
 import { Button } from "@/components/ui/button";
 import {
   Dialog,
@@ -12,56 +9,60 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { useState } from "react"; 
+import { Label } from "../ui/label";
+import { Input } from "../ui/input";
+import { useState } from "react";
 
 const TodoCard = (props) => {
-
-   const [dialogIsVisible,setDialogIsVisible]= useState(false)
-   
-   function handlerEdit(){
-    
-   }
+  const [changedTodo, setChangedTodo] = useState(null);
 
   return (
-    <>
+    <div>
       <div className="flex justify-between items-center rounded-md">
-        <h1 className="text-xl font-mono">{props.item}</h1>
+        <h1 className="text-xl font-mono">{props.todo}</h1>
         <div className="space-x-5 flex h-10 ">
-          
-          <Button onClick={()=>setDialogIsVisible(true)} variant="secondary">Edit</Button>
-          <Separator orientation="vertical" className="" />
-          <AlertDialogItem onDelete={props.onDelete.bind(null, props.id)} />
-        </div>
+          <Dialog>
+            <DialogTrigger>
+              <Button variant="secondary">Edit</Button>
+            </DialogTrigger>
+            <DialogContent>
+              <DialogHeader>
+                <DialogDescription>
+                  Make changes to your Todo here. Click save when you're done.
+                </DialogDescription>
+              </DialogHeader>
 
+              <Label htmlFor="todo" className="text-right">
+                Todo
+              </Label>
+              <Input
+                onChange={(e) => setChangedTodo(e.target.value)}
+                id="todo"
+                defaultValue={props.todo}
+              />
+
+              <DialogFooter>
+                <Button
+                  onClick={() => props.onUpdateTodo(props.id, changedTodo)}
+                  type="submit"
+                >
+                  Save changes
+                </Button>
+              </DialogFooter>
+            </DialogContent>
+          </Dialog>
+
+          <Button
+            onClick={() => props.onDeleteTodo(props.id)}
+            variant="secondary"
+          >
+            Delete
+          </Button>
+          <Separator orientation="vertical" className="" />
+        </div>
       </div>
       <Separator />
-      <Dialog open={dialogIsVisible} onOpenChange={setDialogIsVisible} >
-      
-     
-      <DialogContent className="sm:max-w-[425px]">
-        <DialogHeader>
-          <DialogTitle>Edit Todo</DialogTitle>
-          <DialogDescription>
-            Make changes to your todo here. Click save when you're done.
-          </DialogDescription>
-        </DialogHeader>
-        <div className="grid gap-4 py-4">
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="name" className="text-right">
-              Todo : 
-            </Label>
-            <Input onChange={(e)=> setValue(e.target.value)} id="name" defaultValue={props.item} className="col-span-3" />
-          </div>
-          
-        </div>
-        <DialogFooter>
-          <Button onClick={handlerEdit}>Save changes</Button>
-        </DialogFooter>
-      </DialogContent>
-    </Dialog>
-    </>
+    </div>
   );
 };
 
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,7 @@
-import { AlertDialog, AlertDialogItem } from "@/components/alert/alert-dialog";
 import TodoCard from "@/components/todo/todo-card";
-import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { useRef, useState } from "react";
+import { useState } from "react";
 
 export default function Home() {
   const [todos, setTodos] = useState([
@@ -11,85 +9,47 @@ export default function Home() {
     "Yemek Yap",
     "Haftalık ödevlerini tamamla",
   ]);
+  const [enteredTodo, setEnteredTodo] = useState("");
 
-  const [selectedTodo,setSelectedTodo] = useState('')
-// YUSUF DG
-  const [alertDestructive, setAlertDestructive] = useState(false);
-  const [alertSuccess, setAlertSuccess] = useState(false);
-
-  
-  const todoRef = useRef();
-
-  function handlerAddTodo(e) {
-    const enteredTodo = todoRef.current.value;
+  function submitHandler(e) {
     e.preventDefault();
 
-    if (enteredTodo.length < 5) {
-      return;
-    }
+    setTodos((todos) => [...todos, enteredTodo]);
 
-    setTodos((prev) => [...prev, enteredTodo]);
-
-    setAlertSuccess(true);
-    setTimeout(() => {
-      setAlertSuccess(false);
-    }, 2000);
+    setEnteredTodo("");
   }
 
-  function handlerDeleteTodo(id) {
-    setTodos((item) => item.filter((item, index) => index !== id));
-    setAlertDestructive(true);
-    setTimeout(() => {
-      setAlertDestructive(false);
-    }, 2000);
+  function deleteTodoHandler(id) {
+    setTodos(todos.filter((todo, idx) => idx !== id));
   }
 
-  function handlerEditTodo(item) {
-  //  setSelectedTodo(item);
-  
-  const enteredTodo = todoRef.current.value;
-
+  function updateTodoHandler(id, changedTodo) {
+    console.log(id, changedTodo);
   }
-
-  
 
   return (
     <div className="flex flex-col bg-[#000000] w-full h-screen items-center  text-white">
-      <div className=" absolute right-10 top-20 animate-bounce">
-        {alertDestructive && (
-          <Alert variant="destructive">
-            <AlertTitle>Deleted</AlertTitle>
-            <AlertDescription>Todo is deleted succesfully</AlertDescription>
-          </Alert>
-        )}
-        {alertSuccess && (
-          <Alert>
-            <AlertTitle className="text-green-600">Added</AlertTitle>
-            <AlertDescription className="text-green-600">
-              Todo is added succesfully...
-            </AlertDescription>
-          </Alert>
-        )}
-      </div>
       <h1 className="mt-10 text-[50px] font-semibold font-serif">TODO APP</h1>
-      <form className=" w-3/5 p-10 flex space-x-4 text-2xl  justify-center">
+      <form
+        onSubmit={submitHandler}
+        className=" w-3/5 p-10 flex space-x-4 text-2xl  justify-center"
+      >
         <Input
-         ref={todoRef}
+          value={enteredTodo}
+          onChange={(e) => setEnteredTodo(e.target.value)}
           className=" w-3/6 text-black text-xl"
           placeholder="Please entered a todo..."
         />
-        <Button onClick={handlerAddTodo}>addd</Button>
+        <Button>Add</Button>
       </form>
-      <div className=" w-3/6  shadow-[0_0_10px_0_white] bg-slate-800  p-10 space-y-6 rounded-md max-h-[55%] overflow-y-scroll  ">
-        {todos.map((item, index) => (
+      <div className=" w-fullbg-slate-800 p-10 space-y-6 rounded-md">
+        {todos.map((todo, index) => (
           <TodoCard
-            selectedTodo= {selectedTodo}
-            setSelectedTodo= {setSelectedTodo}
-            onEdit={handlerEditTodo}
-            onDelete={handlerDeleteTodo}
-            key={index}
             id={index}
-            item={item}
+            key={index}
+            todo={todo}
+            onDeleteTodo={deleteTodoHandler}
+            onUpdateTodo={updateTodoHandler}
           />
         ))}
       </div>
